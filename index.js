41734624820f2da7f0ab37024925a4db46cc1042
Benjamin coder <maciel@zenithlacres.com.br>
2026-03-03 11:46:41 -0300

    fix: remover duplicidade gerarResposta, crash resolvido

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const { gerarResposta } = require("./iaBrain");
+
 
 const app = express();
 app.use(cors());
